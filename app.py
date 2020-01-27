0c0a3ac96b6c7a2416d52cbfcc5498fb4b2432f1
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-01-27 12:24:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def predict():
     else:
           return render_template('index.html', prediction_text='RESULT IS NEGATIVE')
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
