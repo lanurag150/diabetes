c8e99cc2a52fbed4c9ebfc077042df710ec3255f
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-05-27 10:01:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,36 @@ def predict():
     output = round(prediction[0], 2)
   
     if(output==1):
-       return render_template('index.php', prediction_text='RESULT IS POSITIVE')
+       return render_template('index.php', prediction_text='<div class="col-lg-6" style="margin-top: 200px">
+      <div class="w3-card-4" style="width:50%;">
+    <header class="w3-container w3-black">
+      <h1 style="color:white">Analysis Report</h1>
+    </header>
+
+    <div class="w3-container">
+      <p id="result">You have been tested 'POSITIVE'</p>
+    </div>
+
+    <footer class="w3-container w3-black">
+      <h5 style="color:white">Note :: All reports are made with 90% accuracy, please contact to our doctor.</h5>
+    </footer>
+  </div>
+	</div>')
     else:
-          return render_template('index.php', prediction_text='RESULT IS NEGATIVE')  
+          return render_template('index.php', prediction_text='  <div class="col-lg-6" style="margin-top: 200px">
+      <div class="w3-card-4" style="width:50%;">
+    <header class="w3-container w3-black">
+      <h1 style="color:white">Analysis Report</h1>
+    </header>
+
+    <div class="w3-container">
+      <p id="result">You have been tested 'Negative'</p>
+    </div>
+
+    <footer class="w3-container w3-black">
+      <h5 style="color:white">Note :: All reports are made with 90% accuracy, we're glad to inform you that you're safe.</h5>
+    </footer>
+  </div>
+	</div>')  
 if __name__ == "__main__":
     app.run(debug=True)
