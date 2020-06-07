ec6970733fbe5f62a8836cf148edf38fc7ca4b5e
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-06-07 10:22:26 +0530

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -28,6 +28,31 @@
     <link rel="stylesheet" href="{{ url_for('static', filename='css/css/style.css') }}" >
     <link rel="stylesheet" href="{{ url_for('static', filename='css/css/flaticon.css') }}" >
     <link rel="stylesheet" href="{{ url_for('static', filename='css/css/icomoon.css') }}">
+	    <style>
+.button {
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button1 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #4CAF50;
+}
+
+.button1:hover {
+  background-color: #4CAF50;
+  color: white;
+}
+	  </style>
   </head>
   <body>
   
@@ -120,6 +145,7 @@
             </div>
             </div>
       {{ prediction_text }}
+	   <a href="http://premediccare.rf.gd/Malaria_rec.php?i=1" target="_top" align="right">  <button class="button button1">Recommend</button></a>
     </section>
 		<footer class="ftco-footer ftco-bg-dark ftco-section img" style="background-image: url(images/bg_5.jpg);">
     	<div class="overlay"></div>
